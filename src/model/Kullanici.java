d083e2e6eeae6384a5565e797f4138327b11daa8
RECEP HUROL <recephurol@hotmail.com>
2022-12-16 01:07:46 +0300

    gitignore ve db create script eklendi 2

--- src/model/Kullanici.java
+++ src/model/Kullanici.java
@@ -7,4 +7,40 @@ public class Kullanici {
     private String kullaniciAdi;
     private String sifre;
     private EnumKullaniciTipi kullaniciTipi;
+
+    public Kullanici(){
+
+    }
+    public int getId(){
+        return id;
+    }
+
+    public String getKullaniciAdi(){
+        return kullaniciAdi;
+    }
+
+    public void setKullaniciAdi(String kullaniciAdi){
+        kullaniciAdi = kullaniciAdi;
+    }
+
+
+    public String getSifre(){
+        return sifre;
+    }
+
+    public void setSifre(String sifre){
+        sifre = sifre;
+    }
+
+
+    public EnumKullaniciTipi getKullaniciTipi(){
+        return kullaniciTipi;
+    }
+
+    public void setKullaniciTipi(EnumKullaniciTipi kullaniciTipi){
+        kullaniciTipi = kullaniciTipi;
+    }
+
+
+
 }
