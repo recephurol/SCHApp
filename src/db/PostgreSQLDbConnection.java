6b87918dea9505f1935852cd2fe864b48e907c4e
recep.hurol <recep.hurol@hmb.gov.tr>
2022-12-23 21:34:04 +0300

    urun ekle urun var mi kontrolu

--- src/db/PostgreSQLDbConnection.java
+++ src/db/PostgreSQLDbConnection.java
@@ -236,21 +236,40 @@ public class PostgreSQLDbConnection extends DbConnection {
     public void urunEkle(Urun urun) throws SQLException {
         if(conn!=null){
             try {
-                String insertUrunQuery ="INSERT INTO urun (deleted, adi, kategori_id, marka_id,renk_id,aciklama) VALUES ( ?, ?, ?, ?, ?,?);";
+                ResultSet urunId=null;
+                ResultSet result = urunGetir(urun.get_adi(),urun.get_kategoriId(),urun.get_markaId(),urun.get_renkId(),urun.get_magazaId());
+                while(result.next()){
 
-                PreparedStatement insertQuery = conn.prepareStatement(insertUrunQuery,Statement.RETURN_GENERATED_KEYS);
-                insertQuery.setBoolean(1,false);
-                insertQuery.setString(2,urun.get_adi());
-                insertQuery.setInt(3,urun.get_kategoriId());
-                insertQuery.setInt(4, urun.get_markaId());
-                insertQuery.setInt(5,urun.get_renkId());
-                insertQuery.setString(6,urun.get_aciklama());
-                int count = insertQuery.executeUpdate();
+                    if(result==null){
 
-                ResultSet urunId = insertQuery.getGeneratedKeys();
-                while(urunId.next()){
-                    urunFiyatEkle(urunId.getInt("id"),urun.get_magazaId(),urun.get_fiyat(),urun.get_stok());
+                        String insertUrunQuery ="INSERT INTO urun (deleted, adi, kategori_id, marka_id,renk_id,aciklama) VALUES ( ?, ?, ?, ?, ?,?);";
+
+                        PreparedStatement insertQuery = conn.prepareStatement(insertUrunQuery,Statement.RETURN_GENERATED_KEYS);
+                        insertQuery.setBoolean(1,false);
+                        insertQuery.setString(2,urun.get_adi());
+                        insertQuery.setInt(3,urun.get_kategoriId());
+                        insertQuery.setInt(4, urun.get_markaId());
+                        insertQuery.setInt(5,urun.get_renkId());
+                        insertQuery.setString(6,urun.get_aciklama());
+                        int count = insertQuery.executeUpdate();
+
+                        urunId = insertQuery.getGeneratedKeys();
+                    } else {
+                        ResultSet urunFiyatVarMi = urunFiyatGetir(result.getInt("id"),urun.get_magazaId());
+                        if(urunFiyatVarMi==null){
+                            while(urunId.next()){
+                                urunFiyatEkle(urunId.getInt("id"),urun.get_magazaId(),urun.get_fiyat(),urun.get_stok());
+                            }
+                        }
+                    }
+                    if(urunId!=null){
+                        while(urunId.next()){
+                            urunFiyatEkle(urunId.getInt("id"),urun.get_magazaId(),urun.get_fiyat(),urun.get_stok());
+                        }
+                    }
                 }
+
+
 
             } catch (SQLException e) {
             }
@@ -295,6 +314,47 @@ public class PostgreSQLDbConnection extends DbConnection {
             }
 
         }
+    }
+
+    private ResultSet urunGetir(String urunAdi,Integer kategoriId, Integer markaId, Integer renkId, Integer magazaId ){
+        if(conn!=null){
+            Statement myStat = null;
+            try {
+                myStat = conn.createStatement();
+                String query ="select urun.id urunId,urun.adi urunAdi from urun " +
+                        " inner join urun_fiyat uf on urun.id = uf.urun_id " +
+                        " where urun.adi= '"+urunAdi+"' and " +
+                        "      urun.kategori_id= "+kategoriId+" and " +
+                        "      urun.marka_id= "+markaId+" and " +
+                        "      urun.renk_id= "+renkId+" and " +
+                        "      magaza_id= "+magazaId;
+
+                ResultSet urunFiyatListesi= myStat.executeQuery(query);
+                return urunFiyatListesi;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+        return null;
+    }
+
+    private ResultSet urunFiyatGetir(Integer urunId, Integer magazaId ){
+        if(conn!=null){
+            Statement myStat = null;
+            try {
+                myStat = conn.createStatement();
+                String query =" select id, urun_Id,magaza_id  from urun_fiyat " +
+                        " where magaza_id="+magazaId+" and urun_id="+urunId;
+
+                ResultSet urunFiyat= myStat.executeQuery(query);
+                return urunFiyat;
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+        return null;
     }
 
 
